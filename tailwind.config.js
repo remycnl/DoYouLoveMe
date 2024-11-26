be234b2a05b6cad2b9d2ed1abbb7e377dba107df
Rémy Canal <remy.canal@epitech.eu>
2024-11-26 18:34:35 +0100

    [add] V1.0.0

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+          black: "#111319",
+          white: "#FAF9F6",
+          primary: "#FFEECC",
+          secondary: "#FFDDCC",
+          third: "#FEBBCC",
+      },
+  },
+  },
+  plugins: [],
+}
